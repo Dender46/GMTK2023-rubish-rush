fca5ce04186e0005ec3194f20593a4dda1ba031d
Dender46 <dmitriy.bagandov@gmail.com>
2023-07-09 18:43:46 +0300

    Add audio for window

--- Assets/Scripts/WindowStatus.cs
+++ Assets/Scripts/WindowStatus.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class WindowStatus : MonoBehaviour
 {
     [SerializeField] List<Material> m_ShatteredGlassMats;
+    [SerializeField] List<AudioClip> m_Sounds;
 
     bool m_IsShattered = false;
     public void Shatter()
@@ -18,6 +19,13 @@ public class WindowStatus : MonoBehaviour
 
         GameManager.ShowBonusText(transform.position);
 
+        PLaySound();
+
         m_IsShattered = true;
+    }
+    void PLaySound()
+    {
+        var sound = m_Sounds[Random.Range(0, m_Sounds.Count)];
+        GetComponent<AudioSource>().PlayOneShot(sound);
     }
 }
