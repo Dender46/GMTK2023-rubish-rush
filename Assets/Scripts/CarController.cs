80daa61d03fd623f73e8bedfa62f692fb19a80a6
Dender46 <dmitriy.bagandov@gmail.com>
2023-07-09 19:00:00 +0300

    Add sounds to truck

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,14 +7,23 @@ public class CarController : MonoBehaviour
     [SerializeField] float m_MaxSpeedForward = 20.0f;
     [SerializeField] float m_MaxSpeedBackward = -20.0f;
     [SerializeField] float m_RotationSpeed = 20.0f;
+    [Header("Audio")]
+    [SerializeField] AudioClip m_AudioReversing;
+    [SerializeField] float m_IdlePitch = 0.6f;
+    [SerializeField] float m_RunningPitch = 1.0f;
 
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    AudioSource m_AudioSourceReversing;
     float m_Velocity = 0.0f;
     float m_RotationY = 0.0f;
 
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+        m_AudioSourceReversing = GameObject.Find("AudioSourceReversing").GetComponent<AudioSource>();
+        m_AudioSourceReversing.Stop();
     }
 
     void Update()
@@ -24,20 +33,27 @@ public class CarController : MonoBehaviour
         var steering = Input.GetAxis("Horizontal");
         
         m_Velocity += acceleration * m_Speed * dt;
-        if (m_Velocity > 0.0f)
-        {
-            m_Velocity -= m_Decceleration * dt;
-        }
-        else if (m_Velocity < 0.0f)
-        {
-            m_Velocity += m_Decceleration * dt;
-        }
+        m_Velocity += m_Velocity > 0.0f ? -m_Decceleration * dt : m_Decceleration * dt;
         m_Velocity = Mathf.Clamp(m_Velocity, m_MaxSpeedBackward, m_MaxSpeedForward);
+
 
         if (m_Rigidbody.velocity.magnitude > 0.0f)
         {
             m_RotationY += steering * m_RotationSpeed * m_Rigidbody.velocity.magnitude * dt;
             transform.rotation = Quaternion.Euler(0.0f, m_RotationY, 0.0f);
+        }
+
+        m_AudioSource.pitch = Mathf.Lerp(m_IdlePitch, m_RunningPitch, m_Velocity / m_MaxSpeedForward);
+        if (acceleration < 0.0f)
+        {
+            if (!m_AudioSourceReversing.isPlaying)
+            {
+                m_AudioSourceReversing.PlayOneShot(m_AudioReversing);
+            }
+        }
+        else
+        {
+            m_AudioSourceReversing.Stop();
         }
     }
 
